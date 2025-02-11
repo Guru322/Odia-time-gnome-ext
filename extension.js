b79c3f6756610d1b77508a740ec79de376c21fcf
GURU SENSEI <sahoogurucharan00@gmail.com>
2025-02-11 15:41:19 +0530

    Create extension.js

---
+++ extension.js
@@ -0,0 +1,74 @@
+const Main = imports.ui.main;
+const St = imports.gi.St;
+const GLib = imports.gi.GLib;
+
+let originalClock;    
+let customClock;     
+let clockUpdateId;    
+
+function init() {
+}
+
+function enable() {
+    originalClock = Main.panel.statusArea.dateMenu.actor;
+    originalClock.hide();
+
+    customClock = new St.Label({
+        text: "",
+        style_class: "panel-clock" 
+    });
+
+    Main.panel._centerBox.insert_child_at_index(customClock, 0);
+
+    updateClock();
+    clockUpdateId = GLib.timeout_add_seconds(GLib.PRIORITY_DEFAULT, 60, () => {
+        updateClock();
+        return GLib.SOURCE_CONTINUE;
+    });
+}
+
+function disable() {
+    if (clockUpdateId) {
+        GLib.source_remove(clockUpdateId);
+        clockUpdateId = null;
+    }
+
+    if (customClock) {
+        Main.panel._centerBox.remove_child(customClock);
+        customClock.destroy();
+        customClock = null;
+    }
+
+    if (originalClock) {
+        originalClock.show();
+    }
+}
+
+function updateClock() {
+    let now = GLib.DateTime.new_now_local();
+
+    let hour = now.get_hour();
+    let minute = now.get_minute();
+    let odiaTime = toOdiaDigits(`${hour}:${minute}`);
+
+    let weekdayIndex = now.get_day_of_week();  
+    let monthIndex = now.get_month() - 1;     
+    let day = now.get_day();
+    let odiaDay = toOdiaDigits(day.toString());
+
+    let odiaWeekdays = ["ରବି", "ସୋମ", "ମଙ୍ଗଳ", "ବୁଧ", "ଗୁରୁ", "ଶୁକ୍ର", "ଶନି"];
+    let odiaMonths = ["ଜାନୁ", "ଫେବୃ", "ମାର୍ଚ୍", "ଅପ୍ରେ", "ମଇ", "ଜୁନ", "ଜୁଲା", "ଅଗ", "ସେପ୍", "ଅକ୍", "ନଭ", "ଡିସେ"];
+
+    let odiaWeekday = odiaWeekdays[weekdayIndex - 1];
+    let odiaMonth = odiaMonths[monthIndex];
+
+    let formattedDate = `${odiaWeekday}, ${odiaDay} ${odiaMonth}`;
+    let finalText = `${odiaTime} • ${formattedDate}`;
+
+    customClock.set_text(finalText);
+}
+
+function toOdiaDigits(str) {
+    let odiaDigits = ["୦", "୧", "୨", "୩", "୪", "୫", "୬", "୭", "୮", "୯"];
+    return str.replace(/\d/g, d => odiaDigits[parseInt(d)]);
+}
